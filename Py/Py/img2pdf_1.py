7f7e500dfbadcc930db7d9e2ddca5284c1cfea8a
Dongcheng WANG <tony.wangdongcheng@qq.com>
2019-11-15 15:43:49 +0800

    another img2pdf

--- Py/Py/img2pdf_1.py
+++ Py/Py/img2pdf_1.py
@@ -4,14 +4,14 @@ import os
 
 def pic2pdf():
     doc = fitz.open()
-    for img in sorted(glob.glob("d:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test\\*.png")):  # 读取图片，确保按文件名排序
+    for img in sorted(glob.glob("d:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test666\\*.png")):  # 读取图片，确保按文件名排序
         print(img)
         imgdoc = fitz.open(img)                 # 打开图片
         pdfbytes = imgdoc.convertToPDF()        # 使用图片创建单页的 PDF
         imgpdf = fitz.open("pdf", pdfbytes)
         doc.insertPDF(imgpdf)                   # 将当前页插入文档
 
-    pdfpath = 'd:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test\\out.pdf'
+    pdfpath = 'd:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test666\\out.pdf'
 
     if os.path.exists(pdfpath):
         os.remove(pdfpath)
---
+++ Py/Py/img2pdf_2.py
@@ -0,0 +1,42 @@
+# https://github.com/josch/img2pdf
+
+import img2pdf
+
+pdfpath = 'd:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test666\\out_2.pdf'
+img1    = 'd:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test666\\Frontpage_1.png'
+img2    = 'd:\\My Documents\\BaiduCloud\\SICOD\\GitHub\\Py\\test666\\Frontpage_2.png'
+# multiple inputs (variant 1)
+with open(pdfpath,"wb") as f:
+	f.write(img2pdf.convert(img1, img2))
+
+#import img2pdf
+
+## opening from filename
+#with open("name.pdf","wb") as f:
+#	f.write(img2pdf.convert('test.jpg'))
+
+## opening from file handle
+#with open("name.pdf","wb") as f1, open("test.jpg") as f2:
+#	f1.write(img2pdf.convert(f2))
+
+## using in-memory image data
+#with open("name.pdf","wb") as f:
+#	f.write(img2pdf.convert("\x89PNG...")
+
+## multiple inputs (variant 1)
+#with open("name.pdf","wb") as f:
+#	f.write(img2pdf.convert("test1.jpg", "test2.png"))
+
+## multiple inputs (variant 2)
+#with open("name.pdf","wb") as f:
+#	f.write(img2pdf.convert(["test1.jpg", "test2.png"]))
+
+## writing to file descriptor
+#with open("name.pdf","wb") as f1, open("test.jpg") as f2:
+#	img2pdf.convert(f2, outputstream=f1)
+
+## specify paper size (A4)
+#a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+#layout_fun = img2pdf.get_layout_fun(a4inpt)
+#with open("name.pdf","wb") as f:
+#	f.write(img2pdf.convert('test.jpg', layout_fun=layout_fun))
