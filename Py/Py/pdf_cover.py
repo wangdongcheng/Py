22d9f2cc7e967624f8a92bc4caeb8d78ccbd9421
Dongcheng WANG <tony.wangdongcheng@qq.com>
2019-11-18 17:00:44 +0800

    cover sections with black blocks on imgs successfully

--- Py/Py/pdf_cover.py
+++ Py/Py/pdf_cover.py
@@ -14,6 +14,7 @@ def get_pdf_files(pdf_path):
     return pdf_files
 
 def pdf_to_imgs(pdf_files):
+    pdf_imgs_full_path = []
     zoom = 200
     rotate = 0
     pdf_imgs = []
@@ -32,13 +33,50 @@ def pdf_to_imgs(pdf_files):
 
         dummy,pdf_file_name = os.path.split(pdf_file)
         pdf_img_full_path = pdf_img_path + re.sub('.[Pp][Dd][Ff]','_',pdf_file_name)+'.png'
+        pdf_imgs_full_path.append(pdf_img_full_path)
         pix_map.writePNG(pdf_img_full_path)
         pdf_img = numpy.array(Image.open(pdf_img_full_path))
-        list.append(pdf_imgs,pdf_img)
+        pdf_imgs.append(pdf_img)
     
-    return pdf_imgs
+    return pdf_imgs,pdf_imgs_full_path
+
+def cover_method(pdf_img, start_x, start_y, end_x, end_y, pdf_img_full_path):
+    for x in range(int(start_x), int(end_x) + 1):
+        for y in range(int(start_y), int(end_y) + 1):
+            pdf_img[x, y] = 0
+    pdf_img = Image.fromarray(pdf_img.astype("uint8"))
+    pdf_img.save(pdf_img_full_path)
+
+
+def img_cover(pdf_imgs,pdf_imgs_full_path):
+    coordinate_config = open('coordinate.txt', 'r')
+    start_x = []
+    start_y = []
+    end_x = []
+    end_y = []
+
+    for line in coordinate_config:
+        points = line.split()
+        start_x.append(points[0])
+        start_y.append(points[1])
+        end_x.append(points[2])
+        end_y.append(points[3])
+        
+    for i in range(0, len(pdf_imgs)):
+        for j in range(0, len(start_x)):
+            cover_method(pdf_imgs[i],start_x[j],
+                                     start_y[j],
+                                     end_x[j],
+                                     end_y[j],
+                                     pdf_imgs_full_path[i])
+
 
 if __name__ == '__main__':
+    pdf_imgs = pdf_imgs_full_path = []
     pdf_path = input('Please input the PDF folder path:')
+    if pdf_path == '':
+        pdf_path = 'test666/' # default pdf path
+        print('PDF path is:', pdf_path)
     pdf_files = get_pdf_files(pdf_path)
-    pdf_imgs = pdf_to_imgs(pdf_files)
+    pdf_imgs,pdf_imgs_full_path = pdf_to_imgs(pdf_files)
+    img_cover(pdf_imgs,pdf_imgs_full_path)
