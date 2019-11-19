7ab3fc6c9656e5c4ee191d22a0463e60ed175528
Dongcheng WANG <tony.wangdongcheng@qq.com>
2019-11-19 18:13:31 +0800

    pylint

--- Py/Py/pdf_cover.py
+++ Py/Py/pdf_cover.py
@@ -1,7 +1,7 @@
 import os
 import glob
-import fitz
 import re
+import fitz
 import numpy
 from PIL import Image
 import img2pdf
@@ -20,8 +20,8 @@ def get_pdf_files(pdf_path):
 # =========================================================
 def pdf_to_imgs(pdf_files):
     pdf_imgs_full_path = []
-    zoom = 200
-    rotate = 0
+    ZOOM = 200
+    ROTATE = 0
     pdf_imgs = []
     pdf_img_path, dummy = os.path.split(pdf_files[0])
     pdf_img_path = pdf_img_path + '/img/'
@@ -34,9 +34,10 @@ def pdf_to_imgs(pdf_files):
         dummy, pdf_file_name = os.path.split(pdf_file)
         for i in range(0, pdf_doc.pageCount):
             pdf_page = pdf_doc[i]
-            pdf_trans = fitz.Matrix(zoom / 100.0, zoom / 100.0).preRotate(rotate)
+            pdf_trans = fitz.Matrix(ZOOM / 100.0, ZOOM / 100.0).preRotate(ROTATE)
             pix_map = pdf_page.getPixmap(matrix=pdf_trans, alpha=False)
-            pdf_img_full_path = pdf_img_path + re.sub('.[Pp][Dd][Ff]', '_', pdf_file_name) + str(i).zfill(2) +'.png'
+            pdf_img_full_path = (pdf_img_path + re.sub('.[Pp][Dd][Ff]', '_', pdf_file_name) 
+                                 + str(i).zfill(2) +'.png')
             pdf_imgs_full_path.append(pdf_img_full_path)
             pix_map.writePNG(pdf_img_full_path)
             pdf_img = numpy.array(Image.open(pdf_img_full_path))
@@ -47,9 +48,9 @@ def pdf_to_imgs(pdf_files):
 # add black block on the images
 # =========================================================
 def cover_method(pdf_img, start_x, start_y, end_x, end_y, pdf_img_full_path):
-    for x in range(int(start_x), int(end_x)+1):
-        for y in range(int(start_y), int(end_y)+1):
-            pdf_img[y, x] = 0
+    for x_pos in range(int(start_x), int(end_x)+1):
+        for y_pos in range(int(start_y), int(end_y)+1):
+            pdf_img[y_pos, x_pos] = 0
     pdf_img = Image.fromarray(pdf_img.astype("uint8"))
     pdf_img.save(pdf_img_full_path)
 
@@ -71,11 +72,9 @@ def img_cover(pdf_imgs, pdf_imgs_full_path):
         end_y.append(points[3])
     for i in range(0, len(pdf_imgs)):
         for j in range(0, len(start_x)):
-            cover_method(pdf_imgs[i],start_x[j],
-                                     start_y[j],
-                                     end_x[j],
-                                     end_y[j],
-                                     pdf_imgs_full_path[i])
+            cover_method(pdf_imgs[i], start_x[j], start_y[j], 
+                                      end_x[j], end_y[j],
+                                      pdf_imgs_full_path[i])
 
 def img_to_pdf(pdf_path, pdf_files):
     if pdf_path[len(pdf_path) - 1] != '/':
@@ -91,17 +90,21 @@ def img_to_pdf(pdf_path, pdf_files):
         pdf_png_files = glob.glob(pdf_img_path + pdf_filename + '*.png')
         with open(output_fullpath,"wb") as f:
             f.write(img2pdf.convert(pdf_png_files))
-     
+
 # =========================================================
 # Main
 # =========================================================
-if __name__ == '__main__':
+def main():
     pdf_imgs = pdf_imgs_full_path = []
     pdf_path = input('Please input the PDF folder path:')
     if pdf_path == '':
         pdf_path = 'test666/' # default PDF path
         print('PDF path is:', pdf_path)
     pdf_path, pdf_files = get_pdf_files(pdf_path)
-    pdf_imgs,pdf_imgs_full_path = pdf_to_imgs(pdf_files)
+    pdf_imgs, pdf_imgs_full_path = pdf_to_imgs(pdf_files)
     img_cover(pdf_imgs,pdf_imgs_full_path)
     img_to_pdf(pdf_path,pdf_files)
+
+
+if __name__ == '__main__':
+    main()
