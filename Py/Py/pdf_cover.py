90b7bc43725f81bd6e94b7b376d41c0d290c5d02
Dongcheng WANG <tony.wangdongcheng@qq.com>
2019-11-25 12:18:11 +0800

    vs code to format the code

--- Py/Py/pdf_cover.py
+++ Py/Py/pdf_cover.py
@@ -9,6 +9,8 @@ import img2pdf
 # =========================================================
 # get PDF files from given path
 # =========================================================
+
+
 def get_pdf_files(pdf_path):
     if pdf_path[len(pdf_path) - 1] != '/':
         pdf_path = pdf_path + '/'
@@ -18,6 +20,8 @@ def get_pdf_files(pdf_path):
 # =========================================================
 # convert PDF to images
 # =========================================================
+
+
 def pdf_to_imgs(pdf_files):
     pdf_imgs_full_path = []
     ZOOM = 200
@@ -34,10 +38,11 @@ def pdf_to_imgs(pdf_files):
         dummy, pdf_file_name = os.path.split(pdf_file)
         for i in range(0, pdf_doc.pageCount):
             pdf_page = pdf_doc[i]
-            pdf_trans = fitz.Matrix(ZOOM / 100.0, ZOOM / 100.0).preRotate(ROTATE)
+            pdf_trans = fitz.Matrix(
+                ZOOM / 100.0, ZOOM / 100.0).preRotate(ROTATE)
             pix_map = pdf_page.getPixmap(matrix=pdf_trans, alpha=False)
-            pdf_img_full_path = (pdf_img_path + re.sub('.[Pp][Dd][Ff]', '_', pdf_file_name) 
-                                 + str(i).zfill(2) +'.png')
+            pdf_img_full_path = (pdf_img_path + re.sub('.[Pp][Dd][Ff]', '_', pdf_file_name)
+                                 + str(i).zfill(2) + '.png')
             pdf_imgs_full_path.append(pdf_img_full_path)
             pix_map.writePNG(pdf_img_full_path)
             pdf_img = numpy.array(Image.open(pdf_img_full_path))
@@ -47,6 +52,8 @@ def pdf_to_imgs(pdf_files):
 # =========================================================
 # add black block on the images
 # =========================================================
+
+
 def cover_method(pdf_img, start_x, start_y, end_x, end_y, pdf_img_full_path):
     for x_pos in range(int(start_x), int(end_x)+1):
         for y_pos in range(int(start_y), int(end_y)+1):
@@ -57,6 +64,8 @@ def cover_method(pdf_img, start_x, start_y, end_x, end_y, pdf_img_full_path):
 # =========================================================
 # proceed to add black block cover by given coordinates
 # =========================================================
+
+
 def img_cover(pdf_imgs, pdf_imgs_full_path):
     coordinate_config = open('coordinate.txt', 'r')
     start_x = []
@@ -72,9 +81,11 @@ def img_cover(pdf_imgs, pdf_imgs_full_path):
         end_y.append(points[3])
     for i in range(0, len(pdf_imgs)):
         for j in range(0, len(start_x)):
-            cover_method(pdf_imgs[i], start_x[j], start_y[j], 
-                                      end_x[j], end_y[j],
-                                      pdf_imgs_full_path[i])
+            cover_method(pdf_imgs[i],
+                         start_x[j], start_y[j],
+                         end_x[j], end_y[j],
+                         pdf_imgs_full_path[i])
+
 
 def img_to_pdf(pdf_path, pdf_files):
     if pdf_path[len(pdf_path) - 1] != '/':
@@ -88,22 +99,24 @@ def img_to_pdf(pdf_path, pdf_files):
         output_fullpath = output_path + pdf_fullname
         pdf_filename = pdf_fullname.split('.')[0]
         pdf_png_files = glob.glob(pdf_img_path + pdf_filename + '*.png')
-        with open(output_fullpath,"wb") as f:
+        with open(output_fullpath, "wb") as f:
             f.write(img2pdf.convert(pdf_png_files))
 
 # =========================================================
 # Main
 # =========================================================
+
+
 def main():
     pdf_imgs = pdf_imgs_full_path = []
     pdf_path = input('Please input the PDF folder path:')
     if pdf_path == '':
-        pdf_path = 'test666/' # default PDF path
+        pdf_path = 'test666/'  # default PDF path
         print('PDF path is:', pdf_path)
     pdf_path, pdf_files = get_pdf_files(pdf_path)
     pdf_imgs, pdf_imgs_full_path = pdf_to_imgs(pdf_files)
-    img_cover(pdf_imgs,pdf_imgs_full_path)
-    img_to_pdf(pdf_path,pdf_files)
+    img_cover(pdf_imgs, pdf_imgs_full_path)
+    img_to_pdf(pdf_path, pdf_files)
 
 
 if __name__ == '__main__':
